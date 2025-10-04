ef5e1bd9230e5062368332b795182f3b176edfc9
Qy-wy <lizaliyts@gmail.com>
2025-10-04 22:13:37 +0300

    Add task_10.py

---
+++ task_10.py
@@ -0,0 +1,12 @@
+def arifmetics(a, b):
+    print("Sum = ", a + b)
+    print("Diff = ", a - b)
+    print("Product = ", a * b)
+    print("Division = ", a / b)
+    print("Integer division = ", a // b)
+    print("Modulo = ", a % b)
+    print("Power = ", a ** b)
+
+A = int(input("Enter a: "))
+B = int(input("Enter b: "))
+arifmetics(A, B)
