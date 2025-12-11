1557ff9d501e7e46fdb27e15e4b67044a8cc6417
Qy-wy <lizaliyts@gmail.com>
2025-12-11 18:40:41 +0300

    add tsk4 lab4

---
+++ lab4/task_4.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.random.rand(10, 10)
+
+fig, ax = plt.subplots(figsize=(6, 6))
+
+heatmap = ax.imshow(data, cmap='viridis')
+
+ax.set_title('Тепловая карта 10x10', fontsize=14)
+
+plt.tight_layout()
+plt.show()
