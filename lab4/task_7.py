f791c6d6b826e9c1f7c8b0363d8f679177ae3af9
Qy-wy <lizaliyts@gmail.com>
2025-12-11 19:09:22 +0300

    add tsk7 lab4

---
+++ lab4/task_7.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
+
+X, Y = np.meshgrid(x, y)
+Z = np.sin(np.sqrt(X**2 + Y**2))
+
+ax.plot_surface(X, Y, Z, rstride=2, cstride=2, cmap=cm.copper)
+
+ax.set_xlabel('x-axis')
+ax.set_ylabel('y-axis')
+ax.set_zlabel('z-axis')
+
+plt.show()
